7a5f1641c98c9e9f7266628ed93be4514b7214fc
dmtweedy <damon.tweedy@yahoo.com>
2023-08-04 14:31:20 -0500

    added readfile code

--- server.js
+++ server.js
@@ -25,16 +25,46 @@ app.get('*', (res) => {
 
 // api routes
 app.get('/api/notes', (res) => {
-
+  fs.readFile(notesData, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
 });
 
 app.post('/api/notes', (req, res) => {
+  fs.readFile(notesData, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
 
+    const notes = JSON.parse(data);
+    const newNote = {
+      id: notes.length + 1,
+      title: req.body.title,
+      text: req.body.text,
+    };
+    notes.push(newNote);
+
+
+  });
 });
 
 
 app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
 
+  fs.readFile(notesData, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+
+  });
 });
 
 app.listen(PORT, () => {
